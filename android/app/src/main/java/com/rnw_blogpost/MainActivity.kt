b72f6af0ffa4976f5eec860434c77100c6d4d438
mehmet öz <154225219+ozkmehmetozk@users.noreply.github.com>
2023-12-22 23:39:24 +0300

    Update MainActivity.kt

--- android/app/src/main/java/com/rnw_blogpost/MainActivity.kt
+++ android/app/src/main/java/com/rnw_blogpost/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "rnw_blogpost"
+  override fun getMainComponentName(): String = "reactweb"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
